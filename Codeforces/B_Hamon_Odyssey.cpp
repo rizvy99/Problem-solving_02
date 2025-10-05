59ea7cfdf4d703128c8bb4b77f9e23ee6506bd35
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2025-10-05 23:17:38 +0600

    Codeforces Round 1056(Div-2)-A solved

--- Codeforces/B_Hamon_Odyssey.cpp
+++ Codeforces/B_Hamon_Odyssey.cpp
@@ -36,35 +36,7 @@ int main() {
         cnt=max(1,cnt);
         cout<<cnt<<el;
         
-     
     }
     return 0;
 }
-/*int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int cur = arr[0];
-    int part = 1;
-
-    for (int i = 0; i < n; i++) {
-        cur &= arr[i];
-
-        if (cur == 0) {
-            if (i == n - 1) {
-                break;
-            } else {
-                part++;
-                cur = arr[i + 1];
-            }
-        }
-    }
-
-    if (cur != 0) {
-        part--;
-    }
-
-    part = max(part, 1);
-
-    cout << part << '\n';*/
+ 
