77290d91e64e0626aea9c63e90b251f46ca894de
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2026-06-07 18:59:54 +0600

    Solved very  interesting and funny problem

---
+++ Codeforces/A_Construct_an_Array.cpp
@@ -0,0 +1,24 @@
+//Alrazi Hosen Rizvy
+#include<bits/stdc++.h>
+#define ll long long int
+#define el "\n"
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define sortall(x) sort(all(x))
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=1;i<=2*n;i+=2){
+                cout<<i<<" ";
+        }
+        cout<<el;
+    }
+    
+    return 0;
+}
